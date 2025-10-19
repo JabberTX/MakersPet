78e42b87e3ac23aa7c86b9c860e5f1d2c8d6d94e
JabberTX <limeytx@yahoo.com>
2025-10-19 13:04:13 -0500

    Installation scripts for ROS2 Kilted on Ubuntu 24.04

---
+++ scripts/install_kaiaai_kilted.sh
@@ -0,0 +1,88 @@
+#!/bin/bash
+# Install Kaia.ai ROS2 platform
+#   https://github.com/kaiaai/install
+. /opt/ros/iron/setup.bash
+mkdir ~/uros_ws && cd ~/uros_ws
+git clone --depth 1 -b kilted https://github.com/micro-ROS/micro_ros_setup.git src/micro_ros_setup
+sudo apt update
+sudo apt install -y ed python3-pip
+sudo apt install -y ros-kilted-rmw-fastrtps-cpp
+sudo apt remove -y ros-kilted-rmw-cyclonedds-cpp
+sudo rosdep init
+rosdep update --rosdistro kilted
+rosdep install --from-paths src --ignore-src -y
+colcon build
+. install/local_setup.sh
+ros2 run micro_ros_setup create_agent_ws.sh
+ros2 run micro_ros_setup build_agent.sh
+rm -rf log/ build/ src/
+
+cd ~/.ros
+wget https://raw.githubusercontent.com/kaiaai/install/main/ubuntu/etc/disable_fastdds_shm.xml
+wget https://raw.githubusercontent.com/kaiaai/install/main/ubuntu/etc/disable_fastdds_shm_localhost_only.xml
+cd ~
+
+export RMW_IMPLEMENTATION=rmw_fastrtps_cpp
+export MICROROS_DISABLE_SHM=1
+
+cd ~/makerpet_ws
+sudo apt install -y python3-pip nano inotify-tools \
+ros-kilted-ros-gz ros-kilted-joint-state-publisher-gui \
+ros-kilted-cartographer-ros ros-kilted-ros-gz-sim \
+ros-kilted-nav2-map-server ros-kilted-nav2-bringup \
+ros-kilted-joint-state-publisher ros-kilted-xacro
+pip3 install aiortc aiohttp opencv-python
+sudo apt autoremove -y
+# sudo rm -rf /var/lib/apt/lists/*
+
+git clone -b iron --depth 1 https://github.com/kaiaai/kaiaai_msgs src/kaiaai_msgs
+git clone -b iron --depth 1 https://github.com/kaiaai/kaiaai src/kaiaai
+git clone -b iron --depth 1 https://github.com/kaiaai/kaiaai_telemetry src/kaiaai_telemetry
+git clone -b iron --depth 1 https://github.com/kaiaai/kaiaai_teleop src/kaiaai_teleop
+git clone -b iron --depth 1 https://github.com/kaiaai/kaiaai_bringup src/kaiaai_bringup
+git clone -b iron --depth 1 https://github.com/makerspet/makerspet_fido src/makerspet_fido
+git clone -b iron --depth 1 https://github.com/makerspet/makerspet_loki src/makerspet_loki
+git clone -b iron --depth 1 https://github.com/makerspet/makerspet_snoopy src/makerspet_snoopy
+git clone -b iron --depth 1 https://github.com/makerspet/makerspet_mini src/makerspet_mini
+git clone -b iron --depth 1 https://github.com/makerspet/makerspet src/makerspet
+git clone -b iron --depth 1 https://github.com/kaiaai/kaiaai_gazebo src/kaiaai_gazebo
+git clone --depth 1 https://github.com/kaiaai/kaiaai_web src/kaiaai_web
+git clone --depth 1 https://github.com/kaiaai/kaiaai_apps src/kaiaai_apps
+git clone --depth 1 https://github.com/kaiaai/m-explore-ros2 src/m-explore-ros2
+git clone --depth 1 https://github.com/kaiaai/auto_mapper src/auto_mapper
+git clone --depth 1 https://github.com/kaiaai/nav2_wfe src/nav2_wfe
+colcon build --symlink-install
+rm -rf log/
+mkdir ~/maps
+
+cat <<EOF >> ~/.bashrc
+. ~/uros_ws/install/setup.bash
+. ~/makerpet_ws/install/setup.bash
+alias kaia='ros2 run kaiaai cli'
+
+export RMW_IMPLEMENTATION=rmw_fastrtps_cpp
+export MICROROS_DISABLE_SHM=1
+
+if [ "$MICROROS_DISABLE_SHM" = "1" ] ; then
+    if [ "$ROS_LOCALHOST_ONLY" = "1" ] ; then
+        export FASTRTPS_DEFAULT_PROFILES_FILE=~/.ros/disable_fastdds_shm_localhost_only.xml
+    else
+        export FASTRTPS_DEFAULT_PROFILES_FILE=~/.ros/disable_fastdds_shm.xml
+    fi
+fi
+
+EOF
+
+cat <<EOF >> ~/.bash_history
+exit
+ros2 launch kaiaai_bringup navigation.launch.py use_sim_time:=true slam:=True
+ros2 launch kaiaai_gazebo world.launch.py
+ros2 param set /pet lidar.scan.freq.target 7.0
+ros2 launch kaiaai_bringup navigation.launch.py map:=$HOME/maps/map.yaml
+ros2 run nav2_map_server map_saver_cli -f ~/maps/map --ros-args -p save_map_timeout:=60.0
+ros2 launch explore_lite explore.launch.py
+ros2 launch kaiaai_bringup navigation.launch.py slam:=True
+ros2 launch kaiaai_bringup monitor_robot.launch.py
+ros2 run kaiaai_teleop teleop_keyboard
+ros2 launch kaiaai_bringup physical.launch.py
+EOF
